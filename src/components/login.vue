6dcf231ccbfb362c64da5dc5493e09da703d29dd
wellkk <123691380@qq.com>
2019-02-16 23:31:47 +0800

    登录-表单

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,15 +1,30 @@
 <template>
     <div>
-        login------
+        <el-form label-position="top" label-width="80px" :model="formdata">
+            <h2>用户登陆</h2>
+            <el-form-item label="用户名">
+                <el-input v-model="formdata.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="formdata.passwoed"></el-input>
+            </el-form-item>
+            <el-button class="login-btn" type="primary">登录</el-button>
+        </el-form>
     </div>
 </template>
 
 <script>
 export default {
-
-}
+    data(){
+        return {
+            formdata:{
+                username:'',
+                passwoed:''
+            }
+        }
+    }
+};
 </script>
 
 <style>
-
 </style>
