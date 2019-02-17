986ce53fb1b1c2ef2d2ba1cb18c29789f6528abd
wellkk <123691380@qq.com>
2019-02-17 22:17:14 +0800

    登录样式--注册事件

--- src/components/login.vue
+++ src/components/login.vue
@@ -6,24 +6,40 @@
                 <el-input v-model="formdata.username"></el-input>
             </el-form-item>
             <el-form-item label="密码">
-                <el-input v-model="formdata.passwoed"></el-input>
+                <el-input v-model="formdata.password"></el-input>
             </el-form-item>
-            <el-button class="login-btn" type="primary">登录</el-button>
+            <el-button
+            @click.prevent = handlelogin()
+            class="login-btn" type="primary">登录</el-button>
         </el-form>
     </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            formdata:{
-                username:'',
-                passwoed:''
-            }
-        }
+  data () {
+    return {
+      formdata: {
+        username: '',
+        password: ''
+      }
     }
-};
+  },
+  methods: {
+    // 登录请求
+    handlelogin () {
+        this.$http
+        .post(`http://localhost:8888/api/private/v1/login`,this.formdata)
+        .then((res) => {
+          console.log(res)
+        //   console.log(res.data);
+        }).catch(err=>{
+            console.log(err);
+            
+        })
+    }
+  }
+}
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,9 @@ import router from './router'
 import ElementUI from 'element-ui'
 import '@/assets/css/base.css'
 import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 /* eslint-disable no-new */
