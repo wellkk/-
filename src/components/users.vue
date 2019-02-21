16dfc6b2a58b3e0c649e2f8c19213615ba725a8e
wellkk <123691380@qq.com>
2019-02-21 18:11:22 +0800

    添加 - 显示对话框

--- src/components/users.vue
+++ src/components/users.vue
@@ -13,7 +13,7 @@
         <el-input @clear="getAllUsers()" clearable placeholder="请输入内容" v-model="query" class="searchInput">
           <el-button @click="searchUser()" slot="append" icon="el-icon-search"></el-button>
         </el-input>
-        <el-button type="primary">添加用户</el-button>
+        <el-button type="primary" @click="showDiaAddUser()">添加用户</el-button>
       </el-col>
     </el-row>
 
@@ -51,16 +51,31 @@
       </el-table-column>
     </el-table>
     <!-- 分页 -->
-     <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="pagenum"
-      :page-sizes="[2, 4, 6, 8]"
-      :page-size="2"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="total">
+    <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="pagenum" :page-sizes="[2, 4, 6, 8]" :page-size="2" layout="total, sizes, prev, pager, next, jumper" :total="total">
     </el-pagination>
+    <!-- 对话框 添加用户 -->
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisibleAdd">
+      <el-form label-position="left" label-width="80px" :model="formdata">
+        <el-form-item label="用户名">
+          <el-input v-model="formdata.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="formdata.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱">
+          <el-input v-model="formdata.email"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input v-model="formdata.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleAdd = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisibleAdd = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
+
 </template>
 
 <script>
@@ -72,35 +87,48 @@ export default {
       // 当前页码
       pagenum: 2,
       pagesize: 2,
-      total:10
+      total: 10,
+      // 对话框显示与隐藏属性
+      dialogFormVisibleAdd: false,
+      // 添加的表单数据
+      formdata: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
+      }
     };
   },
   created() {
     this.getTableData();
   },
   methods: {
+    // 添加用户 - 显示对话框
+    showDiaAddUser() {
+      this.dialogFormVisibleAdd = true;
+    },
     // 清空查询内容--给input添加属性clearble,再加载所有用户
-    getAllUsers(){
+    getAllUsers() {
       this.getTableData();
     },
     // 查询用户
-    searchUser(){
+    searchUser() {
       this.pagenum = 1;
       // query是双向绑定，参数在getTableData中
       this.getTableData();
     },
     // 分页方法
-     handleSizeChange(val) {
-       this.pagenum = 1;
-        console.log(`每页 ${val} 条`);
-        this.pagesize = val;
-        this.getTableData();
-      },
-      handleCurrentChange(val) {
-        console.log(`当前页: ${val}`);
-         this.pagenum = val;
-         this.getTableData();
-      },
+    handleSizeChange(val) {
+      this.pagenum = 1;
+      console.log(`每页 ${val} 条`);
+      this.pagesize = val;
+      this.getTableData();
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+      this.pagenum = val;
+      this.getTableData();
+    },
     //   获取表单数据方法
     async getTableData() {
       const AUTH_TOKEN = localStorage.getItem("token");
