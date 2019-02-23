adb200ee984c2764e90d7c1f238edad0088175a6
wellkk <123691380@qq.com>
2019-02-24 00:10:54 +0800

    展开行

--- src/components/roles.vue
+++ src/components/roles.vue
@@ -3,6 +3,29 @@
         <cus-bread level1="权限管理" level2="角色列表"> </cus-bread>
         <el-button class="btn" type="primary">添加角色</el-button>
         <el-table :data="roles" style="width: 100%">
+            <el-table-column type="expand">
+                <template slot-scope="scope">
+                    <el-row class="level1" v-for="(item1,i) in scope.row.children" :key="item1.id">
+                        <el-col :span="4">
+                            <el-tag closable type="danger">{{item1.authName}}</el-tag>
+                            <i class="el-icon-arrow-right"></i>
+                        </el-col>
+                        <el-col :span="20">
+                            <el-row v-for="(item2,i) in item1.children" :key="item2.id" class="level2">
+                                <el-col :span="4">
+                                    <el-tag closable>{{item2.authName}}</el-tag>
+                                    <i class="el-icon-arrow-right"></i>
+
+                                </el-col>
+                                <el-col :span="20">
+                                    <el-tag closable type="warning" v-for="(item3,i) in item2.children" :key="item3.id">{{item3.authName}}</el-tag>
+                                </el-col>
+                            </el-row>
+                        </el-col>
+
+                    </el-row>
+                </template>
+            </el-table-column>
             <el-table-column type="index" label="#" width="160">
             </el-table-column>
             <el-table-column prop="roleName" label="角色名称" width="200">
@@ -27,7 +50,7 @@
 export default {
   data() {
     return {
-      roles: [],
+      roles: []
     };
   },
   created() {
@@ -52,5 +75,9 @@ export default {
 }
 .btn {
   margin-top: 20px;
+}
+.level1,
+.level2 {
+  margin-bottom: 10px;
 }
 </style>
